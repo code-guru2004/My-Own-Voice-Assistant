3386537e5683680131e3256abee66dfb8be209a9
Nayan Das <138520442+code-guru2004@users.noreply.github.com>
2024-04-14 00:44:22 +0530

    Create speak.py

---
+++ speak.py
@@ -0,0 +1,23 @@
+
+import pyttsx3 
+  
+def onStart(): 
+   print('starting') 
+  
+def onWord(name, location, length): 
+   print('word', name, location, length) 
+  
+def onEnd(name, completed): 
+   print('finishing', name, completed) 
+  
+engine = pyttsx3.init() 
+  
+engine.connect('started-utterance', onStart) 
+engine.connect('started-word', onWord) 
+engine.connect('finished-utterance', onEnd) 
+  
+sen = 'Geeks for geeks is a computer portal for Geeks'
+  
+  
+engine.say(sen) 
+engine.runAndWait()
